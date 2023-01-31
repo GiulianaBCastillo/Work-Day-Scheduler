3586f3276a26f0052d33f3e338a563ed8475e462
GiulianaBCastillo <giuliana.b.castillo@gmail.com>
2023-01-31 15:53:51 +0000

    color code modified, 'Save' buttons created, LocalStorage added

--- assets/script.js
+++ assets/script.js
@@ -11,12 +11,48 @@ let rows = document.getElementsByClassName(
 );
 let currentHour = parseInt(moment().format("H"));
 
+function loadStorage(){
+    var savedTasks = JSON.parse(localStorage.getItem("savedTasks"))
+    console.log(savedTasks)
+    if(savedTasks == null){
+        localStorage.setItem("savedTasks", JSON.stringify([null,null,null,null,null,null,null,null,null]))
+        return
+    }
+    // We will need to render it soon!
+    var inputElements = document.getElementsByTagName("input")
+    console.log(inputElements)
+    for(let i=0; i< inputElements.length; i++){
+        if(savedTasks[i] != null){
+            inputElements[i].value = savedTasks[i]
+        }
+    }
+}
+
+
+var saveButtons = document.getElementsByClassName("save")
+console.log(saveButtons)
+
+for(let i=0; i < saveButtons.length; i++){
+    saveButtons[i].addEventListener("click", function(event){
+        event.preventDefault()
+        var savedTasks = JSON.parse(localStorage.getItem("savedTasks"))
+        console.log(savedTasks)
+        console.log(this.previousElementSibling.value)
+        savedTasks[i] = this.previousElementSibling.value
+        console.log(savedTasks)
+        localStorage.setItem("savedTasks", JSON.stringify(savedTasks))
+    })
+}
+
 Array.from(rows).forEach((row) => {
   let rowIdString = row.id,
     rowHour;
   if (rowIdString) {
     rowHour = parseInt(rowIdString);
   }
+  console.log(row)
+  console.log(rowHour)
+  console.log(currentHour, "Current hour")
   if (rowHour) {
     // Compares row id to current hour and sets color accordingly
     if (currentHour === rowHour) {
@@ -34,3 +70,6 @@ Array.from(rows).forEach((row) => {
 function setColor(element, color) {
   element.style.backgroundColor = color;
 }
+
+
+loadStorage()
